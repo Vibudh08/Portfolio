a30abb4c929f2bf488e948d67aac565388bb56b4
Vibudh08 <vibudhrathore8@gmail.com>
2023-08-30 12:15:27 +0530

    30-08

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,6 @@ import {AiOutlineClose} from 'react-icons/ai'
 
 const Navbar = () => {
   const [hum,setHum]=useState(true);
-  const changebtn=()=>{
-    setHum(!hum)
-  }
 
   return (
     <nav className="navbar">
@@ -16,15 +13,15 @@ const Navbar = () => {
         <h3><span>V</span>IBUDH</h3>
       </div>
       <div className="links">
-        <div className="bigscrn">
-        <Link to={'/'}>Home</Link>
-        <Link to={'/project'}>Projects</Link>
-        <Link to={'/about'}>About me</Link>
-        <Link to={'/contact'}>Contact me</Link>
-        </div>
-        <div className="hicon" onClick={changebtn}>
-          {hum?<GiHamburgerMenu/>:<AiOutlineClose/>}     
+        <ul className={hum ? 'navul' :'navul navul-mobile' }>
+          <li><Link to={'/'}>Home</Link></li>
+          <li><Link to={'/about'}>About me</Link></li>
+          <li><Link to={'/project'}>Projects</Link></li>
+          <li><Link to={'/contact'}>Contact me</Link></li>
+        </ul>
       </div>
+      <div className="hicon" onClick={()=>setHum(!hum)}>
+          {hum? <AiOutlineClose/> :<GiHamburgerMenu/>}     
       </div>
     </nav>
   );
