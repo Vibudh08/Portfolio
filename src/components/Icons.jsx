0d9d98adc17655486fd5ef7eab40daca8cf6ab85
Vibudh08 <vibudhrathore8@gmail.com>
2024-01-06 15:39:04 +0530

    hr

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import {Link} from 'react-router-dom'
 
-import {FaGithub, FaLinkedin, } from 'react-icons/fa'
-import './Icons.css'
+import { FaGithub, FaLinkedin, FaHackerrank } from "react-icons/fa";
+import { LiaHackerrank } from "react-icons/lia";
+import "./Icons.css";
 
 const Icons = () => {
   return (
     <div className="icons">
-      <a href='https://github.com/Vibudh08' target = "_blank"><FaGithub className="git icn" /></a>
-      <a href='https://www.linkedin.com/in/vibudh08' target = "_blank"><FaLinkedin className="linked icn" /></a>
+      <a href="https://github.com/Vibudh08" target="_blank">
+        <FaGithub className="git icn" />
+      </a>
+      <a href="https://www.linkedin.com/in/vibudh08" target="_blank">
+        <FaLinkedin className="linked icn" />
+      </a>
+      <a href="https://www.hackerrank.com/dashboard" target="_blank">
+        <LiaHackerrank className="linked icn" />
+      </a>
     </div>
   );
 };
